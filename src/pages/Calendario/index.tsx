3d28904838f992d6bf9c85710c0facfe19702404
igorbmeirelles <igorbm.23@gmail.com>
2024-12-06 22:00:10 -0300

    feat: calendar click

--- src/pages/Calendario/index.tsx
+++ src/pages/Calendario/index.tsx
@@ -1,22 +1,34 @@
 import { Link } from "react-router-dom";
 import { ArrowBackIcon } from "../../components/icons/back-arrow";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+
+const daysOfWeek = ["Dom", "Seg", "Ter", "Qua", "Qui", "SEX", "SAB"];
 
 export function Calendario() {
+  const today = useMemo(() => new Date(), []);
+
+  const [month, setMonth] = useState(today.getMonth());
+
+  const [selectedDate, setSelectedDate] = useState(today);
+  console.log(selectedDate);
   const monthDays = useMemo(() => {
-    const today = new Date(Date.now());
     const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+    const lastDayOfMonth = new Date(
+      today.getFullYear(),
+      today.getMonth() + 1,
+      0
+    );
 
-    console.log("today", today);
-
-    const dates = [];
+    const dates = [] as { day: number; fullDate: Date }[];
 
     if (firstDayOfMonth.getDay() !== 1) {
       for (let i = firstDayOfMonth.getDay() - 1; i > 1; i--) {
         const complementaryDate = new Date(firstDayOfMonth);
         complementaryDate.setDate(-i);
-        dates.push(complementaryDate.getDate());
+        dates.push({
+          day: complementaryDate.getDate(),
+          fullDate: complementaryDate,
+        });
       }
 
       console.log("last day", lastDayOfMonth);
@@ -25,23 +37,42 @@ export function Calendario() {
         const date = new Date(firstDayOfMonth);
         date.setDate(i);
 
-        dates.push(date.getDate());
+        dates.push({ day: date.getDate(), fullDate: date });
       }
 
       if (lastDayOfMonth.getDay() !== 7) {
-        console.log(lastDayOfMonth.getDay())
         let j = 0;
-        for (let i = lastDayOfMonth.getDay(); i < 8; i++) {
+        for (let i = lastDayOfMonth.getDay(); i < 6; i++) {
           const complementaryDate = new Date(lastDayOfMonth);
           complementaryDate.setDate(complementaryDate.getDate() + j);
-          dates.push(complementaryDate.getDate());
+          dates.push({
+            day: complementaryDate.getDate(),
+            fullDate: complementaryDate,
+          });
           j++;
         }
       }
 
       return dates;
     }
-  }, []);
+  }, [today]);
+
+  const isCurrentDate = (aDateString: string) => {
+    return new Date(aDateString).getDate() === today.getDate();
+  };
+
+  const isCurrentMonth = (date: Date) => {
+    return date.getMonth() === today.getMonth();
+  };
+
+  const isSelectedDate = (date: Date) => {
+    return date.toDateString() === selectedDate.toDateString();
+  };
+
+  const selectDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setSelectedDate(new Date(event.target.value));
+  };
   return (
     <>
       <header className="gradient-purple p-4 h-[256px]">
@@ -52,13 +83,40 @@ export function Calendario() {
           Datas de vacinação
         </div>
       </header>
-      <div className="bg-white -mt-[172px] gap-4 rounded-t-[52px] pt-12 px-4 grid grid-cols-7">
-        {monthDays?.map((date, index) => (
-          <div key={index} className="p-4 border-b border-gray-200">
-            <p className="text-sm text-gray-500">{date}</p>
-          </div>
+      <form className="bg-white -mt-[172px] rounded-t-[52px] pt-12 gap-2 px-4 grid grid-cols-7">
+        {daysOfWeek.map((day) => (
+          <label key={day} className="text-sm p-2 text-center text-gray-500">
+            {day.toUpperCase()}
+          </label>
         ))}
-      </div>
+
+        {monthDays?.map((date) => (
+          <label
+            key={date.fullDate.toDateString()}
+            htmlFor={date.fullDate.toDateString()}
+            className={`text-sm p-2 relative max-w-[36px] max-h-[36px] aspect-square flex items-center justify-center mx-auto ${
+              isCurrentDate(date.fullDate.toString()) &&
+              !isSelectedDate(date.fullDate)
+                ? "border-[#713EA2] border-b-4 rounded-sm"
+                : ""
+            } ${
+              isSelectedDate(date.fullDate)
+                ? "bg-[#AC85D0] text-white rounded-full"
+                : ""
+            } ${!isCurrentMonth(date.fullDate) ? "text-gray-300" : ""}`}
+          >
+            <input
+              id={date.fullDate.toDateString()}
+              className="absolute inset-0 appearance-none"
+              type="radio"
+              value={date.fullDate.toDateString()}
+              disabled={!isCurrentMonth(date.fullDate)}
+              onChange={selectDate}
+            />
+            {date.day}
+          </label>
+        ))}
+      </form>
     </>
   );
 }
