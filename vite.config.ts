d4a575df3fdd35ba735c915a4063d32baab40982
igorbmeirelles <igorbm.23@gmail.com>
2025-10-21 17:56:15 -0300

    pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,30 @@ import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Mais Vacinas",
+        short_name: "Mais Vacinas",
+        description: "Aplicativo Mais Vacinas",
+        theme_color: "#F1FCFF",
+        icons: [
+          {
+            src: "./public/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./public/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
